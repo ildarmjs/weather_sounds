04cd090457aa3cf863c88934fe95e564dee360ea
Ildar <ildar.1795@gmail.com>
2024-05-14 12:59:21 +0400

    chore: конфигурация для общей сборки

---
+++ webpack.config.common.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: './index.js',
+	output: {
+		fileName: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.resolve(__dirname, 'public/index.html')
+		}),
+		new MiniCssExtractPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [require('postcss-preset-env')]
+							}
+						}
+					},
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf|mp3|jpg|svg)$/i,
+				type: 'asset/resource'
+			}
+		]
+	}
+}
