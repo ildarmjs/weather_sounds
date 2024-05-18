333702eb527b0fe3a523d50383ec804ad6afea22
Ildar <ildar.1795@gmail.com>
2024-05-18 10:45:31 +0400

    feat: импорт изображений и иконок в формате модуля

---
+++ src/images.ts
@@ -0,0 +1,13 @@
+import imageSummer from '../public/summer-bg.jpg'
+import imageRainy from '../public/rainy-bg.jpg'
+import imageWinter from '../public/winter-bg.jpg'
+import iconPause from '../public/icons/pause.svg'
+
+const images: { [key: string]: Object } = {
+	summer: imageSummer,
+	rain: imageRainy,
+	winter: imageWinter,
+	pause: iconPause
+}
+
+export default images
